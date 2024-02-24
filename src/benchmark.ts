96001256fd080baeb3792e1ed80c2b9b203110e7
qwwqe <f.werth@gmail.com>
2024-02-25 01:26:29 +0800

    Define BaseRedisBenchmark

--- src/benchmark.ts
+++ src/benchmark.ts
@@ -1,5 +1,17 @@
-export interface RedisBenchmark {
+import Redis from 'ioredis'
+
+export default interface RedisBenchmark {
     initialize(): Promise<void>
     memoryUsage(): Promise<number>
     cleanUp(): Promise<void>
+}
+
+export abstract class BaseRedisBenchmark implements RedisBenchmark {
+    redis: Redis = new Redis()
+
+    abstract initialize(): Promise<void>
+
+    abstract memoryUsage(): Promise<number>
+
+    abstract cleanUp(): Promise<void>
 }
