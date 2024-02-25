850e60756fc98a3ec374587c7d46b8e11937127a
qwwqe <f.werth@gmail.com>
2024-02-25 18:08:00 +0800

    Flesh out initialization code

--- src/setperprefix.ts
+++ src/setperprefix.ts
@@ -1,10 +1,45 @@
+import { ChainableCommander } from "ioredis";
 import { BaseBenchmark } from "./benchmark";
 
 export default class SetPerPrefix extends BaseBenchmark {
-    async initialize(): Promise<void> {
-        const values = this.generateCJK(
-            this.options.maxKeyLength,
-            this.options.initKeys
-        );
-    }
+  private scoreSetName = `${this.options.keyPrefix}:scores` as const;
+
+  private keys: Set<string> = new Set([this.scoreSetName]);
+
+  async initialize(): Promise<void> {
+    await this.connected;
+
+    const completions = this.generateCJK(
+      this.options.maxKeyLength,
+      this.options.initKeys
+    );
+    const scores = completions.map(() => Math.floor(Math.random() * 10000));
+
+    await this.batchedPipeline(
+      (function* (thiz: SetPerPrefix) {
+        for (let i = 0; i < completions.length; i++) {
+          const completion = completions[i];
+          const score = scores[i];
+
+          for (let j = 0; j < completion.length; j++) {
+            const prefix = completion.slice(0, j + 1);
+            const key = `${thiz.options.keyPrefix}:${prefix}`;
+            thiz.keys.add(key);
+
+            yield (pipeline: ChainableCommander) => {
+              pipeline.zadd(thiz.scoreSetName, score, key);
+            };
+          }
+        }
+      })(this)
+    );
+  }
+
+  async memoryUsage(): Promise<number> {
+    return 0;
+  }
+
+  async cleanUp(): Promise<number> {
+    return 0;
+  }
 }
