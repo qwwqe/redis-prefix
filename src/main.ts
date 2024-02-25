1a805e55b75c3f9984d91f806e938ea1349db991
qwwqe <f.werth@gmail.com>
2024-02-25 19:54:37 +0800

    Update commands

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import SetPerPrefix from "./setperprefix";
 import Benchmark from "./benchmark";
+import CompressedSetPerPrefix from "./compressedsetperprefix";
+import SingleSet from "./singleset";
 
-async function setPerPrefix() {
-  const benchmark: Benchmark = new SetPerPrefix();
-
+async function run(benchmark: Benchmark) {
   await benchmark.initialize();
   const memoryUsage = await benchmark.memoryUsage();
   const deletedKeys = await benchmark.cleanUp();
 
-  console.log(`Memory usage: ${memoryUsage}`);
+  console.log(
+    `Memory usage: ${Math.floor((memoryUsage / 1024 / 1024) * 100) / 100} MB`
+  );
   console.log(`Deleted keys: ${deletedKeys}`);
 }
 
-setPerPrefix();
+const commands: { [command: string]: () => any } = {
+  setPerPrefix: () => run(new SetPerPrefix()),
+  compressedSetPerPrefix: () => run(new CompressedSetPerPrefix()),
+  singleSet: () => run(new SingleSet()),
+};
+
+if (process.argv[2] in commands) {
+  const command = process.argv[2];
+  commands[command]();
+}
